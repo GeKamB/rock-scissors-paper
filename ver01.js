a663e95ddb92c2a3014edc976138ab30697dd81d
GeKamB <k.gebski1@gmail.com>
2023-02-27 21:22:14 +0000

    add buttons,  add eventListener

--- ver01.js
+++ ver01.js
@@ -1,55 +1,105 @@
-const selection = ["rock", "scissors", "paper"];
-let playerscore = 0;
-let computerscore = 0;
 
-// function to get computer choice
-const getComputerChoice = function() {
-    return selection[Math.floor(Math.random() * selection.length)];
-}
-// single round function can be used  for others game where can be only winner/looser and a draw 
+const selection = ["rock", "scissors", "paper"];
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection;
+let computerSelection;
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    playerSelection = e.target.id 
+    computerSelection = ComputerChoice()   
+    playRound(playerSelection, computerSelection)
+    
+
+    
+  })
+})
+
+
+// function to decide computer's random choice
+const ComputerChoice = function () {
+  
+    return selection[Math.floor(Math.random() * selection.length)]
+    
+   
+  }
+    
+// this function is mechanism of a single round
 function playRound(playerSelection, computerSelection) {
   
     switch (playerSelection + computerSelection) {
       case 'scissorspaper':
       case 'rockscissors':
       case 'paperrock':
-         playerscore++;
-         console.log("YOU WIN ! ###  " + playerSelection + " beats " + computerSelection + " player: " + playerscore + " " + "computer: " + computerscore);
+         playerScore++;
+         console.log("YOU WIN ! ###  " +
+                      playerSelection +
+                      " beats " +
+                      computerSelection +
+                      " player: " +
+                      playerScore + 
+                      " " + 
+                      "computer: " +
+                       computerScore);
 
           
         break;
       case 'paperscissors':
       case 'scissorsrock':
       case 'rockpaper':
-        computerscore++;
-        console.log("YOU LOSE ! ###  " + computerSelection + " beats " + playerSelection + " ### player: " + playerscore + " " + "computer: " + computerscore);
+        computerScore++;
+        console.log("YOU LOSE ! ###  " +
+                    computerSelection +
+                    " beats " +
+                    playerSelection +
+                    " ### player: " +
+                     playerScore +
+                     " " +
+                    "computer: " +
+                     computerScore);
         
         break;
       case 'paperpaper':
       case 'scissorsscissors':
       case 'rockrock':
-        console.log("It's a draw ###  " + playerSelection + " : " + computerSelection + " player: " + playerscore + " " + "computer: " + computerscore);
+        console.log("It's a draw ###  " +
+                    playerSelection +
+                     " : " +
+                     computerSelection +
+                     " player: " +
+                     playerScore + 
+                     " " + 
+                     "computer: " + 
+                     computerScore);
       
       break
     }
   }
+
+  
     
   
    
  
-//five rounds of the game 
-
-function game() {
+//This function plays five rounds of the playGame function.
+/*
+function playGame() {
 
   for (let i = 0; i < 5; i++) {
     const playerSelection = prompt();
-    const computerSelection = getComputerChoice();
+    const computerSelection = ComputerChoice();
     playRound(playerSelection, computerSelection);
+
+   
 }
 
 }
 
-game()
+playGame() */
 
 
 
